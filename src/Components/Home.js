acdf3c21ba436c0cde38e45c84727f3ce5f752a5
Dedan Wamalwa <wodedan000@gmail.com>
2023-01-15 00:11:03 +0300

    removed breaks and reduced width in large devices

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
-  return (
-    <div className='row bg-light'>
-        <p>Hello, I'm Dedan Wamalwa</p>
-        <p>I'm a Software Engineer passionate about building user-centric software products
-            that guarantee high user satisfaction. The most amazing experience I've had as SWE 
-            is pair programming-you get to solve problems quite fast!! With that said, I welcome 
-            you to walk with me in this fantastic journey. Feel free to reach out for an engaging session. Thank you.
-        </p>
-    </div>
-  )
+	return (
+		<div className="row bg-secondary text-black">
+			<div className="col-md-7 ml-5">
+				<p className="mb-0">Hello, I'm Dedan Wamalwa </p>
+				<p>I'm a Software Engineer passionate about building user-centric 
+					software products that guarantee high user satisfaction. The most
+					 amazing experience I've had as SWE is pair programming-you get to 
+					 solve problems quite fast! With that said, I welcome you to walk with
+					  me in this fantastic journey. Feel free to reach out for an engaging
+					   session. Thank you.</p>
+			</div>
+		</div>
+	);
 }
 
-export default Home
+export default Home;
