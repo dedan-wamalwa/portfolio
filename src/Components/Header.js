41e529ae56eaeb628cb30c0350b01e0fb17283be
Dedan Wamalwa <wodedan000@gmail.com>
2023-01-18 17:13:27 +0300

    editing links to pages

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,32 +1,37 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Header() {
 	return (
-		<nav className="navbar navbar-expand-md sticky-top">
+		<nav className="navbar navbar-expand-md fixed-top">
 			<div className="container-fluid">
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
-				<a className="navbar-brand fw-bold" href="#">
+				<div className="navbar-brand fw-bold" href="#">
 					Dedan Wamalwa
-				</a>
+				</div>
 				<div className="collapse navbar-collapse " id="navbarNavAltMarkup">
 					<div className="navbar-nav  m-auto">
-						<a className="nav-link active fw-bold " aria-current="page" href="#">
-							Home
-						</a>
-						<a className="nav-link active fw-bold" href="#">
-							About
-						</a>
-						<a className="nav-link active fw-bold" href="#">
-							Experience
-						</a>
-						<a className="nav-link active fw-bold" href="#">
-							Projects
-						</a>
-						<a className="nav-link active fw-bold" href="#">
-							Contact
-						</a>
+						<Link>
+							<div className="nav-link active fw-bold " aria-current="page" href="#">
+								Home
+							</div>
+						</Link>
+						<Link>
+							<div className="nav-link active fw-bold" href="#">
+								Experience
+							</div>
+						</Link>
+						<Link>
+							<div className="nav-link active fw-bold" href="#">
+								Projects
+							</div>
+						</Link>
+						<Link>
+							<div className="nav-link active fw-bold" href="#">
+								Contact
+							</div>
+						</Link>
 					</div>
 				</div>
 			</div>
