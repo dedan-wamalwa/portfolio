50aa20809259b162f2a20fdc7ed5515e19669b50
Dedan Wamalwa <wodedan000@gmail.com>
2023-01-19 00:46:24 +0300

    Edited content

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,48 +1,15 @@
 import React from "react";
-
 function Home() {
 	return (
-		<div className="row bg-secondary text-black">
-			<div className="col-md-7 ml-5 lh-lg">
-				{/* summary */}
-				<p className="mb-0">Hello, I'm Dedan Wamalwa </p>
-				<p className="">
-					I'm a Software Engineer passionate about building user-centric software products that
-					guarantee high user satisfaction. The most amazing experience I've had as SWE is pair
-					programming-you get to solve problems quite fast! With that said, I welcome you to walk
-					with me in this fantastic journey. Feel free to reach out for an engaging session. Thank
-					you.
+		<div className="row text-white content">
+			<div className="col-md-6 m-auto mt-5 fw-bold text-center">
+				<p className="mb-2 mt-5 fs-2 ">Hello, I am Dedan Wamalwa.</p>
+				<p className="fs-4 ">
+					I am an agile minded software engineer passionate about developing user-centric software products.
+					 <br />I appreciate the challenge of bringing an idea to life through lines of code.
 				</p>
-				{/* work experience */}
-				<div className="row">
-					<h5 className="fw-bold">WORK EXPERIENCE</h5>
-					<div className="col-12 col-md-7">Software Engineer - Internship </div>
-					<div className="col-12 col-md-5">September 2022 - January 2023</div>
-					<div className="col-12">
-						<p>
-							● Implemented user experience performance modifications to an existing Point of Sale
-							package; then conducted training sessions on the package and provided support to 10+
-							users.
-						</p>
-						<p>
-							● Collaborated with my team to implement a new feature of the POS software to display
-							a menu that enables customers to order items online.
-						</p>
-						<p>
-							● Implemented a new feature, in collaboration with my team, to a warehouse management
-							and distribution software that tracks towns and customers in a respective route.
-						</p>
-						<p>
-							● Assisted a team member in solving a persistent bug in the database of a warehouse
-							and distribution management software.
-						</p>
-					</div>
-					<div className="col-12 col-md-7">Software Engineer - Internship </div>
-					<div className="col-12 col-md-5">January 2023 - Present</div>
-				</div>
 			</div>
 		</div>
 	);
 }
-
 export default Home;
