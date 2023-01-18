29261ff7596bc2ae151cd6572082c8b4557c103a
Dedan Wamalwa <wodedan000@gmail.com>
2023-01-19 00:47:46 +0300

    Added header and footer components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Experience from './Pages/Experience';
-import Projects from './Pages/Projects';
-import Home from './Pages/Home';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Experience from "./Pages/Experience";
+import Projects from "./Pages/Projects";
+import Home from "./Pages/Home";
 import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 function App() {
 	return (
-		<div className="container-fluid">
+		<div className="container-fluid px-0">
 			<Router>
+				<Header />
 				<Routes>
-					<Route path='/' element={<Home/>}/>
-					<Route path='/' element={<Projects/>}/>
-					<Route path='/' element={<Experience/>}/>
+					<Route path="/" element={<Home />} />
+					<Route path="/Projects" element={<Projects />} />
+					<Route path="/Experience" element={<Experience />} />
+					{/* <Route path="/Contact" element={<Contact />} /> */}
 				</Routes>
+				<Footer />
 			</Router>
 		</div>
 	);
