9676c5e6d14ed26177d091f5307b54860b2f4b43
Dedan Wamalwa <wodedan000@gmail.com>
2023-01-18 17:12:14 +0300

    Routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import Header from './Components/Header';
-import Home from './Components/Home';
-import Footer from './Components/Footer';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Experience from './Pages/Experience';
+import Projects from './Pages/Projects';
+import Home from './Pages/Home';
 import "./App.css";
 function App() {
 	return (
 		<div className="container-fluid">
-			<Header />
-			<Home/>
-			<Footer/>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Home/>}/>
+					<Route path='/' element={<Projects/>}/>
+					<Route path='/' element={<Experience/>}/>
+				</Routes>
+			</Router>
 		</div>
 	);
 }
