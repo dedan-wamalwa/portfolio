1a17b7125a7be22926f8d34f9e5ec5b038aceb52
Dedan Wamalwa <wodedan000@gmail.com>
2023-01-21 14:35:42 +0300

    Spacing

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,15 +3,15 @@ import * as Icon from "react-bootstrap-icons";
 function Home() {
 	return (
 		<div className="row text-white content">
-			<div className="col-md-6 m-auto mt-md-5 mt-1 mb-3 text-center">
+			<div className="col-md-6 m-auto mt-3 mt-md-5 mb-3 text-center">
 				{/* about */}
 				<p className="mb-2 mt-5 fs-2 text-warning fw-bold">Hi, I am Dedan Wamalwa.</p>
-				<p className="fs-4 fw-bold">
+				<p className="fs-4 fw-bold mb-5 mb-md-3">
 					I am a software engineer with an agile mindset passionate about developing user-centric software products.
 					<br />I appreciate the challenge of bringing an idea to life using lines of code.
 				</p>
 				{/* socials */}
-				<p className="text-center">
+				<p className="text-center mb-5 mt-4 mt-md-0">
 					<a href="https://github.com/dedan-wamalwa" target={"_blank"} rel="noopener noreferrer" className="mx-2">
 						<Icon.Github size={30} color={"black"} />
 					</a>
@@ -24,7 +24,7 @@ function Home() {
 				</p>
 				{/* skills */}
 				<h2 className="mt-5 fw-bold" id="skills">Skills</h2>
-				<div className="text-start ps-4 mt-4">
+				<div className="text-start ps-4 mt-2">
 					<h3>Programming Languages</h3>
 					<p className="">Python , JavaScript, PHP, Java.</p>
 					<h3>Front End Development</h3>
