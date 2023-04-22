aad2accad4e4f4a8f288fc105da37028757d5c24
Dedan Wamalwa <wodedan000@gmail.com>
2023-04-23 00:04:22 +0300

    changed layout of projects section

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
 					<Route path="/" element={<Home />} />
 					<Route path="/Projects" element={<Projects />} />
 					<Route path="/Experience" element={<Experience />} />
-					{/* <Route path="/Contact" element={<Contact />} /> */}
 				</Routes>
 				<Footer />
 			</Router>
